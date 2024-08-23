1af8206e09c8668098bf7c52969a3e8ba8165f12
TheP2WKing <tdziomba@hotmail.com>
2024-08-23 21:27:10 +0200

    added slogan

--- src/main/java/net/thep2wking/oedldoedlconstruct/OedldoedlConstruct.java
+++ src/main/java/net/thep2wking/oedldoedlconstruct/OedldoedlConstruct.java
@@ -79,7 +79,7 @@ public class OedldoedlConstruct {
     public static class ModJoinMessage {
         @SubscribeEvent
         public static void addJoinMessage(PlayerLoggedInEvent event) {
-            ModLogInUtil.addJoinMessage(event, NAME, MODID, VERSION);
+            ModLogInUtil.addJoinMessage(event, NAME, MODID, VERSION, true);
         }
     }
 }
